13e06b05fff19795ab587cbf61edb17a6e4fcd36
jackgilbertx <jackgilbertx@gmail.com>
2019-08-27 14:03:37 -0500

    8/27
    destructured style objects

--- src/PieChart.js
+++ src/PieChart.js
@@ -2,7 +2,7 @@ import { ResponsivePie } from "@nivo/pie";
 import React from "react";
 import LegendItem from "./LegendItem";
 import PropTypes from "prop-types";
-import "./styles.css";
+// import "./styles.css";
 
 class PieChart extends React.Component {
   state = {
@@ -13,8 +13,8 @@ class PieChart extends React.Component {
   render() {
     const { data, size, renderCenter } = this.props;
 
-    //creates legend based on data prop
     const legendItems = data.map(legendItem => {
+      //creates legend based on data prop
       return (
         <LegendItem
           legendClick={() =>
@@ -56,7 +56,6 @@ class PieChart extends React.Component {
         fontSize
       },
       centerValueSingleDigit: {
-        //styling to adjust positioning of single digit value
         position: "absolute",
         left: "42.5%",
         top: "45.3%",
@@ -83,10 +82,19 @@ class PieChart extends React.Component {
       }
     };
 
+    const {
+      container,
+      root,
+      chart,
+      centerValue,
+      centerValueSingleDigit,
+      legend
+    } = style;
+
     return (
-      <div style={style.container}>
-        <div style={style.root}>
-          <div style={style.chart}>
+      <div style={container}>
+        <div style={root}>
+          <div style={chart}>
             <ResponsivePie
               data={this.state.data}
               startAngle={-180}
@@ -104,14 +112,14 @@ class PieChart extends React.Component {
           <div //adjusts the position of the chart value to make sure its centered regardless of # of digits
             style={
               this.state.centerValue.length > 2
-                ? style.centerValue
-                : style.centerValueSingleDigit
+                ? centerValue
+                : centerValueSingleDigit
             }
           >
             {renderCenter ? this.state.centerValue : ""}
           </div>
         </div>
-        <div style={style.legend}>{legendItems}</div>
+        <div style={legend}>{legendItems}</div>
         <div />
       </div>
     );
